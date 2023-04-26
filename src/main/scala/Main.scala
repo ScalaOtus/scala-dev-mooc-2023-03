4316235b469d8300d3c091ac808044998a227081
avoronets <avoronets@naumen.ru>
2023-04-27 00:30:57 +0300

    webinar 8

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,11 @@
-import module1.{executor, threads}
+import module1.{executor, future, threads, try_}
 import module1.threads.ToyFuture
 
 import java.util.concurrent.Executor
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
+import scala.language.postfixOps
+import scala.util.Try
 
 object Main {
 
@@ -24,32 +28,59 @@ object Main {
 //    t2.join()
 //    t1.start()
 
-    def rates = {
-      val t1 = threads.getRatesLocation3
-      val t2 = threads.getRatesLocation4
-      println(t1 + t2)
+//    def rates = {
+//      val t1 = threads.getRatesLocation3
+//      val t2 = threads.getRatesLocation4
+//      println(t1 + t2)
+//    }
+
+//    implicit val ex: Executor = ???
+
+//    val t3 = ToyFuture(10)
+//    val t4 = ToyFuture(20)
+
+//    val r: Unit = t3.onComplete{ i1 =>
+//      t4.onComplete{ i2 =>
+//        println(i1 + i2)
+//      }
+//    }
+//
+//    val r2: ToyFuture[Unit] = for{
+//      i1 <- t3
+//      i2 <- t4
+//    } yield println(i1 + i2)
+
+
+
+
+//    threads.printRunningTime(rates)
+
+//    try_.readFromFile().foreach(println)
+
+//    Try("").map(_.toInt).foreach(println)
+
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+
+    def rates2: Future[Int] = {
+      val v1 = future.getRatesLocation1
+      val v2 = future.getRatesLocation2
+      for {
+        r1 <- v1
+        r2 <- v2
+      } yield (r1 + r2)
     }
 
-    implicit val ex: Executor = ???
 
-    val t3 = ToyFuture(10)
-    val t4 = ToyFuture(20)
+    def rates3 =
+      future.getRatesLocation1
+        .zip(future.getRatesLocation2)
 
-    val r: Unit = t3.onComplete{ i1 =>
-      t4.onComplete{ i2 =>
-        println(i1 + i2)
-      }
-    }
+//    future.printRunningTime(rates3)
+//      .foreach(println)
 
-    val r2: ToyFuture[Unit] = for{
-      i1 <- t3
-      i2 <- t4
-    } yield println(i1 + i2)
-
-
-
-
-    threads.printRunningTime(rates)
+    Await.result(future.f7, 6 seconds)
+   // Thread.sleep(4000)
 
   }
 }
--- src/main/scala/module1/07-async.scala
+++ src/main/scala/module1/07-async.scala
@@ -112,13 +112,190 @@ object threads {
 }
 
 object executor {
-      val pool1: ExecutorService = Executors.newFixedThreadPool(2, NameableThreads("fixed-pool-1"))
-      val pool2: ExecutorService = Executors.newCachedThreadPool(NameableThreads("cached-pool-2"))
-      val pool3: ExecutorService = Executors.newWorkStealingPool(4)
-      val pool4: ExecutorService = Executors.newSingleThreadExecutor(NameableThreads("singleThread-pool-4"))
+      val pool1: ExecutorService =
+        Executors.newFixedThreadPool(2, NameableThreads("fixed-pool-1"))
+      val pool2: ExecutorService =
+        Executors.newCachedThreadPool(NameableThreads("cached-pool-2"))
+      val pool3: ExecutorService =
+        Executors.newWorkStealingPool(4)
+      val pool4: ExecutorService =
+        Executors.newSingleThreadExecutor(NameableThreads("singleThread-pool-4"))
 }
 
 
-object future{
+object try_{
 
+  def readFromFile(): List[Int] = {
+    val s: BufferedSource = Source.fromFile(new File("ints.txt"))
+    val result: List[Int] = try{
+      s.getLines().map(_.toInt).toList
+    } catch {
+      case e =>
+        println(e.getMessage)
+        Nil
+    } finally {
+      s.close()
+    }
+    result
+  }
+
+  def readFromFile2(): Try[List[Int]] = {
+    val source: Try[BufferedSource] = Try(Source.fromFile(new File("ints.txt")))
+
+    def lines(s: BufferedSource): Try[List[Int]] =
+      Try(s.getLines().map(_.toInt).toList)
+
+    val result = for{
+      s <- source
+      r <- lines(s)
+    } yield r
+    source.foreach(_.close())
+    result
+  }
+
+  readFromFile2() match {
+    case Failure(exception) =>
+    println(exception.getMessage)
+    case Success(value) =>
+      value.foreach(println)
+  }
+
+  readFromFile2().foreach(l => l.foreach(println))
+
+  val rr = try {
+    println("111")
+    try {
+      throw new Exception("exception 2")
+    } catch{
+      case ex => println(ex.getMessage)
+    }
+  } catch {
+    case ex => println(ex.getMessage)
+  }
+}
+
+object future{
+  // constructors
+
+  def longRunningComputation: Int = ???
+  lazy val f1: Future[Int] = Future.successful(10)
+  lazy val f2: Future[Nothing] = Future.failed(new Exception("oops"))
+  lazy val f3: Future[Int] = Future.fromTry(Try(10))
+  lazy val f4: Future[Int] = Future(longRunningComputation)(scala.concurrent.ExecutionContext.global)
+
+
+
+  // combinators
+
+  //import scala.concurrent.ExecutionContext.Implicits.global
+
+  def getRatesLocation1 = Future{
+    Thread.sleep(1000)
+    println("GetRatesLocation1 "  + Thread.currentThread().getName)
+    10
+  }(scala.concurrent.ExecutionContext.global)
+
+  def getRatesLocation2 = Future{
+    Thread.sleep(2000)
+    println("GetRatesLocation2 "  + Thread.currentThread().getName)
+    20
+  }(scala.concurrent.ExecutionContext.global)
+
+  getRatesLocation1.foreach(println)(scala.concurrent.ExecutionContext.global)
+
+  val _: Future[Int] = getRatesLocation1.recover{
+    case e => 0
+  }(scala.concurrent.ExecutionContext.global)
+
+
+  def printRunningTime[T](v: => Future[T]): Future[T] = {
+    implicit val foo = scala.concurrent.ExecutionContext.global
+    for{
+      start <- Future.successful(System.currentTimeMillis())
+      r <- v
+      end <- Future.successful(System.currentTimeMillis())
+      _ <- Future.successful(println(s"Execution time ${end - start}"))
+    } yield r
+  }
+
+
+
+
+
+
+  // Execution contexts
+  lazy val ec = ExecutionContext.fromExecutor(executor.pool1)
+  lazy val ec2 = ExecutionContext.fromExecutor(executor.pool2)
+  lazy val ec3 = ExecutionContext.fromExecutor(executor.pool3)
+  lazy val ec4 = ExecutionContext.fromExecutor(executor.pool4)
+
+  def action(v: Int): Int = {
+    Thread.sleep(1000)
+    println(s"Action ${v} in ${Thread.currentThread().getName}")
+    v
+  }
+
+  val f5 = Future(action(10))(ec)
+  val f6 = Future(action(20))(ec2)
+
+  val f7 = f5.flatMap{ v1 =>
+    action(50)
+    f6.map{ v2 =>
+      action(v1 + v2)
+    }(ec4)
+  }(ec3)
+
+
+}
+
+object promise {
+
+//  val p1: Promise[Int] = Promise[Int]
+//  p1.isCompleted // false
+//  val f1: Future[Int] = p1.future
+//  f1.isCompleted // false
+//
+//  f1.onComplete()
+//
+//  p1.complete(Try(10))
+
+
+  object FutureSyntax {
+
+
+    def map[T, B](future: Future[T])(f: T => B)(implicit ec: ExecutionContext): Future[B] =
+      flatMap(future)(v => make(f(v)))
+
+    def flatMap[T, B](future: Future[T])(f: T => Future[B])(implicit ec: ExecutionContext): Future[B] = {
+      val p = Promise[B]
+      future.onComplete {
+        case Failure(exception) => p.failure(exception)
+        case Success(value) => f(value).onComplete {
+          case Failure(exception) => p.failure(exception)
+          case Success(value) => p.success(value)
+        }
+      }
+      p.future
+    }
+
+
+    def make[T](v: => T)(implicit ec: ExecutionContext): Future[T] = {
+      val p = Promise[T]
+      val r = new Runnable {
+        override def run(): Unit = p.complete(Try(v))
+      }
+      ec.execute(r)
+      p.future
+    }
+
+    def make[T](v: => T, timeout: Long): Future[T] = {
+      val p = Promise[T]
+      val timer = new Timer(true)
+      val task = new TimerTask {
+        override def run(): Unit = ???
+      }
+      timer.schedule(task, timeout)
+      ???
+    }
+  }
 }
