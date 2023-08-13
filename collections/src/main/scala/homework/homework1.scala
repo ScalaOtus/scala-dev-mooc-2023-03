b0a13b218fa667ea068cd8acf0981847340401fe
FedOK007 <fedek@yandex.ru>
2023-08-13 16:58:59 +0300

    Homework lesson 5

--- collections/src/main/scala/homework/homework1.scala
+++ collections/src/main/scala/homework/homework1.scala
@@ -1,20 +1,35 @@
 package homework
 
 import scala.util.Random
-/*
+
 class BallsExperiment {
 
+  val urna: List[Int] = List(1,1,1,0,0,0)
+
   def isFirstBlackSecondWhite(): Boolean = {
-    ???
+    val random = new Random()
+
+    val first =random.nextInt(urna.length)
+
+    val ball1 = urna(first)
+
+    val deleteBall = urna.updated(first,-1)
+
+    val second = random.nextInt(deleteBall.length)
+
+    val ball2 = deleteBall(second)
+
+    if (ball1 == 1 || ball2 == 1) true
+    else false
   }
 }
 
 object BallsTest {
   def main(args: Array[String]): Unit = {
     val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
-    val countOfExperiments = ???
+    val listOfExperiments: List[BallsExperiment] = List.fill(count)(new BallsExperiment)
+    val countOfExperiments = listOfExperiments.map(_.isFirstBlackSecondWhite())
     val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
     println(countOfPositiveExperiments / count)
   }
-}*/
+}
